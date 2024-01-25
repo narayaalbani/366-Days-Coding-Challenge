882a645cbfc424ebdaa1f52b7d5fc1cc40151add
narayaalbani <naraya.albani@gmail.com>
2024-01-25 20:55:03 +0700

    Update deskripsi Day 1/366

--- src/d1_grow.java
+++ src/d1_grow.java
@@ -1,5 +1,9 @@
-import java.util.Arrays;
+//Day 1/366
 
+//https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 public class d1_grow {
     public static int grow(int[] x) {
         int result = x[0];
