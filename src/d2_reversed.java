dfa6b3c915f5fff816aea6afb7b1352d21f1fa0f
narayaalbani <naraya.albani@gmail.com>
2024-01-25 23:23:13 +0700

    Ganti file Day 2/366

--- src/d2_reversed.java
+++ src/d2_reversed.java
@@ -4,7 +4,7 @@
 
 //Build a function that returns an array of integers from n to 1 where n>0.
 //Example : n=5 --> [5,4,3,2,1]
-public class d2_reversedSequence {
+public class d2_reversed {
     public static int[] reverse(int n){
         int [] result = new int[n];
         for (int i = 0; i < n; i++) {
