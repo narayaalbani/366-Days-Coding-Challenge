d0b1020efea6113f479bd8b704983c5f32d27be1
narayaalbani <naraya.albani@gmail.com>
2024-01-25 20:56:21 +0700

    Update deskripsi Day 2/366

--- src/d2_reversedSequence.java
+++ src/d2_reversedSequence.java
@@ -1,3 +1,9 @@
+//Day 2/366
+
+//https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+//Build a function that returns an array of integers from n to 1 where n>0.
+//Example : n=5 --> [5,4,3,2,1]
 public class d2_reversedSequence {
     public static int[] reverse(int n){
         int [] result = new int[n];
