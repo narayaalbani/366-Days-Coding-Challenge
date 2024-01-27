7db08887cddf6acf6f08851eaa5bd7ffe2e0a77c
narayaalbani <naraya.albani@gmail.com>
2024-01-27 23:04:10 +0700

    Day 10/366

--- test/TestChallenge.java
+++ test/TestChallenge.java
@@ -114,4 +114,21 @@ class TestChallenge {
             assertEquals("1 sheep...2 sheep...3 sheep...", d9_countSheep.countingSheep(3));
         }
     }
+
+    @Nested
+    class d10_minMaxTest {
+        private final d10_maxMin kata;
+
+        d10_minMaxTest() {
+            kata = new d10_maxMin();
+        }
+
+        @Test
+        public void testExamples() {
+            assertEquals(-110, kata.min(new int[]{-52, 56, 30, 29, -54, 0, -110}));
+            assertEquals(0, kata.min(new int[]{42, 54, 65, 87, 0}));
+            assertEquals(566, kata.max(new int[]{4,6,2,1,9,63,-134,566}));
+            assertEquals(5, kata.max(new int[]{5}));
+        }
+    }
 }
